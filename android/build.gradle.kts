78f20a33ee117781d72f0f98865ec69a02980e4a
mKowol <mateuszkowoln@gmail.com>
2023-06-14 20:45:57 +0200

    initial commit

---
+++ android/build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    id("org.jetbrains.compose")
+    id("com.android.application")
+    kotlin("android")
+}
+
+group = "com.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(project(":common"))
+    implementation("androidx.activity:activity-compose:1.6.1")
+}
+
+android {
+    compileSdkVersion(33)
+    defaultConfig {
+        applicationId = "com.example.android"
+        minSdkVersion(24)
+        targetSdkVersion(33)
+        versionCode = 1
+        versionName = "1.0-SNAPSHOT"
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+}
+
+tasks.register("BuildAndRun") {
+    doFirst {
+        exec {
+            workingDir(projectDir.parentFile)
+            commandLine("./gradlew", "android:build")
+            commandLine("./gradlew", "android:installDebug")
+        }
+    }
+}
---
+++ common/build.gradle.kts
@@ -0,0 +1,77 @@
+plugins {
+    kotlin("multiplatform")
+    id("org.jetbrains.compose")
+    id("com.android.library")
+}
+
+group = "com.example"
+version = "1.0-SNAPSHOT"
+
+@OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
+kotlin {
+    android()
+    jvm("desktop")
+    js(IR) {
+        browser()
+    }
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                api(compose.runtime)
+                api(compose.ui)
+                api(compose.foundation)
+                api(compose.materialIconsExtended)
+                api(compose.material3)
+            }
+        }
+
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+
+        val androidMain by getting {
+            dependencies {
+                api("androidx.appcompat:appcompat:1.6.0")
+                api("androidx.core:core-ktx:1.9.0")
+            }
+        }
+
+        val androidTest by getting {
+            dependencies {
+                implementation("junit:junit:4.13.2")
+            }
+        }
+
+        val desktopMain by getting {
+            dependencies {
+                api(compose.preview)
+            }
+        }
+
+        val desktopTest by getting
+
+        val jsMain by getting {
+            dependencies {
+                api(compose.web.core)
+            }
+        }
+
+    }
+
+    explicitApi()
+}
+
+android {
+    compileSdk = 33
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    defaultConfig {
+        minSdk = 24
+        targetSdk = 33
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
